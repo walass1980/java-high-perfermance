a33b3b241c3769bc8e515c8b9b7fed6af2c91049
walass <cha_wal100@yahoo.fr>
2025-08-27 00:07:38 +0200

    up

--- chapter01/Example4.java
+++ chapter01/Example4.java
@@ -18,5 +18,19 @@ public class Example4 {
         for (int number : numbers) {
             System.out.println(number);
         }
+
+        // Remove an element by value
+        numbers.remove(Integer.valueOf(8)); // Remove the element '8'
+
+        // Checked if a LinkedList contains an element
+        boolean containsThree = numbers.contains(3);
+        System.out.println("LinkedList contains 3: " + containsThree);
+        boolean containsEight = numbers.contains(8);
+        System.out.println("LinkedList contains 3: " + containsEight);
+        
+        // Iterate through the LinkedList
+        for (int number : numbers) {
+            System.out.println(number);
+        }
     }
 }
