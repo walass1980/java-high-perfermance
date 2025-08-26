f13f1f043b289acadf9ed5b8868b71b5ace025ae
walass <cha_wal100@yahoo.fr>
2025-08-26 22:45:54 +0200

    up

---
+++ chapter01/Example3.java
@@ -0,0 +1,13 @@
+
+import java.util.Vector;
+
+public class Example3 {
+    public static void main(String[] args) {
+        // Create a victor of integers
+        Vector<Integer> luckyNumbers = new Vector<>();
+
+        // Add elements to the Vector
+        luckyNumbers.add(8);
+        luckyNumbers.add(19);
+    }
+}
