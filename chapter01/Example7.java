e2c9e089ac5bf643305e36a7b871d553ac4b29c3
walass <cha_wal100@yahoo.fr>
2025-08-27 11:14:43 +0200

    up

--- chapter01/Example7.java
+++ chapter01/Example7.java
@@ -16,4 +16,23 @@ class Example7BinarySearchTree {
     public Example7BinarySearchTree() {
         root = null;
     }
+
+    public void insert(int data) {
+     insertRec(root, data);
+    }
+
+    private TreeNode insertRec(TreeNode root, int data) {
+        if (root == null) {
+            root = new TreeNode(data);
+            return root;
+        }
+
+        if (data < root.data) {
+            root.left = insertRec(root.left, data);
+        } else if (data > root.data) {
+            root.right = insertRec(root.right, data);
+        }
+
+        return root;
+    }
 }
