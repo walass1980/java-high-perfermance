c1ff80931c1e3cdb94ebdb965e552b870acd17c5
walass <cha_wal100@yahoo.fr>
2025-08-29 19:54:01 +0200

    up

---
+++ Example11.java
@@ -0,0 +1,15 @@
+public class Example11 {
+    private double[] queue;
+    private int front;
+    private int rear;
+    private int size;
+    private int capacity;
+    public Example11(int capacity) {
+        this.capacity = capacity;
+        front = 0;
+        rear = -1;
+        size = 0;
+    }
+
+    
+}
