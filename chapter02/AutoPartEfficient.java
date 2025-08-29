00efc543ed3021b94fe77a5ad2b9c73a099155f6
walass <cha_wal100@yahoo.fr>
2025-08-29 20:16:29 +0200

    up

---
+++ chapter02/AutoPartEfficient.java
@@ -0,0 +1,16 @@
+// This Java application is provided without data, only
+// Suffient code to illustrate the points made in 
+// chapter 3.
+
+import java.util.List;
+
+public class AutoPartEfficient {
+    public static void main(String[] args) {
+        List<String> autoParts = getAutoParts();
+        processtoParts(autoParts);
+    }
+
+    private static List<String> getAutoParts() {
+        // No
+    }
+}
