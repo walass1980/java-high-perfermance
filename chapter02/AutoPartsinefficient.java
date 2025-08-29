41ffad15befad82e268b61c830168c3f2595cda3
walass <cha_wal100@yahoo.fr>
2025-08-29 20:26:01 +0200

    up

---
+++ chapter02/AutoPartsinefficient.java
@@ -0,0 +1,24 @@
+// This Java applications is provided without data, only
+// sufffincient code to illustrate the points made in
+// chapter 3
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AutoPartsinefficient {
+    public static void main(String[] args) {
+        List<String> autoParts = getAutoParts(); // You will need to populate this array
+        for(String part : autoParts) {
+            processPart(part);
+        }
+    }
+
+    private static List<String> getAutoParts() {
+        // Not implemented; provided for structure.
+        return new ArrayList<>();
+    }
+
+    private static void processPart(String part) {
+        // Not implemented; provided for structure
+    }
+}
