fd68f4c2fd86a0ecf0db74306e2a914332b4929c
walass <cha_wal100@yahoo.fr>
2025-08-29 19:26:06 +0200

    up

--- chapter01/Example7.java
+++ chapter01/Example7.java
@@ -5,8 +5,8 @@ class TreeNode {
 
     public TreeNode(int data) {
         this.data = data;
-        this.left = left;
-        this.right = right;
+        this.left = null;
+        this.right = null;
     }
 }
 
@@ -26,11 +26,72 @@ class Example7BinarySearchTree {
             root = new TreeNode(data);
             return root;
         }
+
         if (data < root.data) {
             root.left = insertRec(root.left, data);
         } else if (data > root.data) {
             root.right = insertRec(root.right, data);
         }
+
         return root;
+    }
+
+    public void inOrderTraversal() {
+        inOrderTraversal(root);
+    }
+
+    private void inOrderTraversal(TreeNode root) {
+        if (root != null) {
+            inOrderTraversal(root.left);
+            System.out.print(root.data + " ");
+            inOrderTraversal(root.right);
+        }
+    }
+
+    public boolean search(int data) {
+        return searchRec(root, data);
+    }
+
+    private boolean searchRec(TreeNode root, int data) {
+        if (root == null) {
+            return false;
+        }
+
+        if (data == root.data) {
+            return true;
+        }
+
+        if (data < root.data) {
+            return searchRec(root.left, data);
+        } else {
+            return searchRec(root.right, data);
+        }
+    }
+}
+
+public class Example7 {
+    public static void main(String[] args) {
+        Example7BinarySearchTree bst = new Example7BinarySearchTree();
+
+        // Insert elements into the binary search tree
+        bst.insert(50);
+        bst.insert(30);
+        bst.insert(70);
+        bst.insert(20);
+        bst.insert(40);
+        bst.insert(60);
+        bst.insert(80);
+
+        // Perform an in-order traversal to print elements in sorted order
+        System.out.println("In-order traversal:");
+        bst.inOrderTraversal();
+
+        // Search for an element in the tree
+        int searchElement = 70;
+        if (bst.search(searchElement)) {
+            System.out.println("\n" + searchElement + " was found in the tree.");
+        } else {
+            System.out.println("\n" + searchElement + " was not found in the tree.");
+        }
     }
 }
