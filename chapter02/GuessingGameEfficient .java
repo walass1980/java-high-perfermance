0c07ab13dca5aac548af59f3661ef30755911cdf
walass <cha_wal100@yahoo.fr>
2025-08-29 21:21:14 +0200

    up

--- chapter02/GuessingGameEfficient .java
+++ chapter02/GuessingGameEfficient .java
@@ -1,34 +1,34 @@
-// This Java applications is provided without data, only
-// sufficient code to illustrate the points made in
+// This Java application is provided without data, only
+// sufficient code to illustrate the points made in 
 // chapter 3.
-
 import java.util.Random;
 import java.util.Scanner;
 
-public class GuessingGameEfficient  {
+public class GuessingGameEfficient {
     public static void main(String[] args) {
         int secretNumber = generateSecretNumber();
-        int userrGess;
+        int userGuess;
 
         do {
-            userrGess = getUserGuess();
-            if(userrGess != secretNumber) {
-                System.err.println("Try again");
+            userGuess = getUserGuess();
+            if (userGuess != secretNumber) {
+                System.out.println("Try again!");
             }
-        } while (userrGess != secretNumber);
+        } while (userGuess != secretNumber);
 
-        System.err.println("You Guessed the secret number:" + secretNumber);
+        System.out.println("You guessed the secret number: " + secretNumber);
     }
 
     private static int generateSecretNumber() {
-        Random random= new Random();
-        return random.nextInt(100) + 1;
+        Random random = new Random();
+        return random.nextInt(100) + 1; 
     }
 
     private static int getUserGuess() {
-        try (Scanner scanner = new Scanner(System.in)){
-            System.out.println("Enter your guess: ");
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Enter your guess: ");
             return scanner.nextInt();
         }
     }
+    
 }
