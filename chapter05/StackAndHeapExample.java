e5bcb745a6e516d1af371c652fb835f8e7ff8ee5
Walass <walass@mbp-de-walass.home>
2025-09-02 16:25:08 +0200

    up

---
+++ chapter05/StackAndHeapExample.java
@@ -0,0 +1,29 @@
+package chapter05;
+
+public class StackAndHeapExample {
+    // Methods to recursively call itself to produce a StackOverflowError
+    public static void causeStackOverflow() {
+        causeStackOverflow();
+    }
+
+    // Method to allocate memory to cause an OutOfMemoryError
+    public static void causeOutOfMemory() {
+        long[] largeArray = new long[Integer.MAX_VALUE];
+    }
+
+    public static void main(String[] args) {
+        // Catch StackOverflowError
+        try {
+            causeStackOverflow();
+        } catch (StackOverflowError e) { 
+           System.out.println("StackOverflowError caught");
+    }
+
+    // Catch OutOfMemoryError
+        try {
+            causeOutOfMemory();
+        } catch (OutOfMemoryError e) {
+            System.out.println("Caught OutOfMemoryError");
+        }
+    }
+}
