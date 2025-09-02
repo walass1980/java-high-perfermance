24050ff532d1c969f30a12deda6c157ab64dcde1
Walass <walass@mbp-de-walass.home>
2025-09-02 16:31:59 +0200

    up

---
+++ chapter06/ConcatenationAlternativeOne.java
@@ -0,0 +1,22 @@
+package chapter06;
+
+public class ConcatenationAlternativeOne {
+    public static void main(String[] args) {
+        // Create a StringBuilder object
+    StringBuilder myStringBuilder = new StringBuilder();
+
+    // Append strings
+    myStringBuilder.append("java");
+    myStringBuilder.append("is");
+    myStringBuilder.append("my");
+    myStringBuilder.append("dog's");
+    myStringBuilder.append("name.");
+
+    // Convert StringBuilder to String
+    String result = myStringBuilder.toString();
+
+    // Print the result
+    System.out.println(result);
+ }
+    
+}
