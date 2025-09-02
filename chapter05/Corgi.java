a272f51f045d0e029bcb7c56bd638e0de2c35c7f
Walass <walass@mbp-de-walass.home>
2025-09-02 16:11:18 +0200

    up

---
+++ chapter05/Corgi.java
@@ -0,0 +1,33 @@
+package chapter05;
+
+public class Corgi {
+    // Instance variables
+    private String name;
+    private int age;
+    private int weight;
+
+    // Constructeur
+    public Corgi(String name, int age, int weight) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+    }
+    
+    // Getter methods
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    
+    
+   
+}
+ 
