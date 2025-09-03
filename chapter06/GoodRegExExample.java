bcfe3a6ff81aca562bc715984280b0ab9797c2db
walass <cha_wal100@yahoo.fr>
2025-09-03 21:53:25 +0200

    up

---
+++ chapter06/GoodRegExExample.java
@@ -0,0 +1,8 @@
+package chapter06;
+
+public class GoodRegExExample {
+    public static void main(String[] args) {
+        // Email pattern regex
+        String emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+    }
+}
