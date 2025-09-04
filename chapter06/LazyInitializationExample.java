68ffc0d549f409c10cceabc70192606a49d10b3e
walass <cha_wal100@yahoo.fr>
2025-09-05 00:11:53 +0200

    up

--- chapter06/LazyInitializationExample.java
+++ chapter06/LazyInitializationExample.java
@@ -1,16 +1,14 @@
-package chapter06;
-
 import java.util.function.Supplier;
 
 public class LazyInitializationExample {
     private Supplier<String> lazyString = () -> {
         // Simulate complex string generation or data retrival
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("Java is incredibbbly ");
-        stringBuilder.append("flexible programming langage ");
+        stringBuilder.append("Java is incredibly ");
+        stringBuilder.append("flexible programming language ");
         stringBuilder.append("text is used in a wide range of application. ");
         stringBuilder.append("This is a complex string ");
-        stringBuilder.append("that was lazely initiated.");
+        stringBuilder.append("that was lazily initiated.");
         return stringBuilder.toString();
     };
 
