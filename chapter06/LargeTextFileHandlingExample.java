22ca7a023ccc839aa62e4e4157fbeaca1d035c5d
walass <cha_wal100@yahoo.fr>
2025-09-04 23:11:49 +0200

    up

---
+++ chapter06/LargeTextFileHandlingExample.java
@@ -0,0 +1,14 @@
+package chapter06;
+
+public class LargeTextFileHandlingExample {
+    public static void main(String[] args) {
+        // Path to the large text file
+        String filePath = "advanced_gide_to_java.txt";
+
+        try {
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+}
