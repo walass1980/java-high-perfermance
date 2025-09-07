01874ebf598210ff6aafc2c7cdfcb3789ea2b4b6
walass <cha_wal100@yahoo.fr>
2025-09-08 00:47:37 +0200

    up

---
+++ chapter10/PostRequestExample.java
@@ -0,0 +1,8 @@
+import java.net.http.HttpClient;
+
+public class PostRequestExample {
+    public static void main(String[] args) {
+        HttpClient client = HttpClient.newHttpClient();
+        String json = "{}";
+    }
+}
